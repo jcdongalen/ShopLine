10e17343762a84306e0437a0dfd6f59e3f9c25a6
John Carlos Dongalen <john_dongalen@medilink.com.ph>
2017-08-07 14:00:35 +0800

    added customer login

--- app/src/main/java/com/pinas/xburner/shopline/Activity/Login.java
+++ app/src/main/java/com/pinas/xburner/shopline/Activity/Login.java
@@ -1,31 +1,57 @@
 package com.pinas.xburner.shopline.Activity;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.pinas.xburner.shopline.R;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
     Button btnLogin;
+    EditText etUserName, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        initialization();
+    }
+
+    private void initialization() {
         btnLogin = (Button) findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(this);
+
+        etUserName = (EditText) findViewById(R.id.etUserName);
+        etUserName.setText("user");
+        etPassword = (EditText) findViewById(R.id.etPassword);
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.btnLogin:
-                startActivity(new Intent(Login.this, MainActivity.class));
+                if(etUserName.getText().toString().equalsIgnoreCase("user")){
+                    startActivity(new Intent(Login.this, MainActivityCustomer.class));
+                }
+                else if(etUserName.getText().toString().equalsIgnoreCase("admin")){
+                    startActivity(new Intent(Login.this, MainActivity.class));
+                }
+                else{
+                    Snackbar snackbar = Snackbar.make(getWindow().getDecorView().findViewById(android.R.id.content), "Incorrect username or password.", Snackbar.LENGTH_LONG);
+                    snackbar.setAction("GOT IT!", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            //Do Nothing
+                        }
+                    });
+                    snackbar.show();
+                }
                 break;
         }
     }
--- app/src/main/java/com/pinas/xburner/shopline/Activity/MainActivityCustomer.java
+++ app/src/main/java/com/pinas/xburner/shopline/Activity/MainActivityCustomer.java
@@ -1,14 +1,24 @@
 package com.pinas.xburner.shopline.Activity;
 
+import android.graphics.Typeface;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.pinas.xburner.shopline.Custom.mTextView;
 import com.pinas.xburner.shopline.Fragments.Completed;
 import com.pinas.xburner.shopline.Fragments.Inquiry;
 import com.pinas.xburner.shopline.Fragments.Shipping;
@@ -18,9 +28,13 @@ import com.pinas.xburner.shopline.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivityCustomer extends AppCompatActivity implements View.OnClickListener {
 
-    private Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    ImageButton btnDrawer, btnMore, btnCart;
+    TextView tvTitle;
+    LinearLayout leftDrawer, rightDrawer;
+
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private int[] tabIcons = {
@@ -29,34 +43,62 @@ public class MainActivity extends AppCompatActivity {
             R.mipmap.ic_shipped_1,
             R.mipmap.ic_completed
     };
+    private String[] tabNames = {"On Sale", "Hot", "Trending", "Brands", "Outdoor", "Sports", "Kids"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_customer);
 
         initialization();
     }
 
-    private void initialization(){
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    private void initialization() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        btnDrawer = (ImageButton) findViewById(R.id.btnDrawer);
+        btnCart = (ImageButton) findViewById(R.id.btnCart);
+        btnMore = (ImageButton) findViewById(R.id.btnMore);
+
+        btnDrawer.setOnClickListener(this);
+        btnCart.setOnClickListener(this);
+
+        leftDrawer = (LinearLayout) findViewById(R.id.leftDrawer);
+        rightDrawer = (LinearLayout) findViewById(R.id.rightDrawer);
+
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
 
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
-        setupTabIcons();
+//        setupTabIcons();
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnDrawer:
+                drawerLayout.openDrawer(leftDrawer);
+                break;
+            case R.id.btnMore:
+                drawerLayout.openDrawer(rightDrawer);
+                break;
+            case R.id.btnCart:
+                break;
+        }
+    }
+
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new Inquiry(), "Inquiry");
         adapter.addFragment(new ToShip(), "To Ship");
         adapter.addFragment(new Shipping(), "Shipping");
+        adapter.addFragment(new Completed(), "Completed");
+        adapter.addFragment(new Completed(), "Completed");
+        adapter.addFragment(new Completed(), "Completed");
         adapter.addFragment(new Completed(), "Completed");
         viewPager.setAdapter(adapter);
     }
@@ -93,7 +135,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public CharSequence getPageTitle(int position) {
-            return null;
+            return tabNames[position];
         }
     }
 }
